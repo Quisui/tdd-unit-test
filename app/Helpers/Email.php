5766d5a8243faf3ac85c97800a20cf8c45da9c99
Enrique <quiqueal.19@gmail.com>
2022-07-05 20:49:03 -0600

    Adding missing configurations

---
+++ app/Helpers/Email.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Helpers;
+
+class Email
+{
+
+    public static function validate($email)
+    {
+        $result = (bool) filter_var($email, FILTER_VALIDATE_EMAIL);
+        return $result;
+    }
+}
